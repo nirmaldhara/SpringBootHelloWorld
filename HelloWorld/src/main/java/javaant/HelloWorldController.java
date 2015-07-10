076ed76ad9a9cd3797af5599c2fa706085af5014
Nirmal Dhara <nirmal.dhara@gmail.com>
2015-07-10 10:08:02 +0530

    Spring boot hello world
    Spring boot hello world

---
+++ HelloWorld/src/main/java/javaant/HelloWorldController.java
@@ -0,0 +1,14 @@
+package javaant;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public @ResponseBody String hello(){
+		return "hello";
+	}
+
+}
